74a2310f57ea1907395090e746011b836479beb4
Suman Das <sum4ndas@gmail.com>
2024-05-11 07:07:47 +0530

    Add router.post() which adds new message and redirects after submitting message

--- routes/index.js
+++ routes/index.js
@@ -27,4 +27,17 @@ router.get("/new", function (req, res) {
   res.render("form");
 });
 
+// Post form
+router.post("/new", function (req, res) {
+  // Add new message to messages array
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.authorName,
+    added: new Date(),
+  });
+
+  // Redirect to home page to after submitting message
+  res.redirect("/");
+});
+
 module.exports = router;
