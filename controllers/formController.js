20cce0e47b8b23ab3a6eb451b8bd1517e3486af1
Suman Das <sum4ndas@gmail.com>
2024-07-26 10:57:20 +0530

    Add fromPost controller to post form data successfully and redirect to '/'

--- controllers/formController.js
+++ controllers/formController.js
@@ -4,4 +4,18 @@ const formGet = (req, res) => {
   });
 };
 
-module.exports = { formGet };
+// Import messages;
+const messages = require("./indexController").messages;
+
+const fromPost = (req, res) => {
+  const text = req.body.message_text;
+  const user = req.body.user;
+
+  // Add new message.
+  messages.push({ text, user, added: new Date() });
+
+  // Redirect to index page.
+  res.redirect("/");
+};
+
+module.exports = { formGet, fromPost };
