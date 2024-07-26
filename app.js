80f3d61baf966140582394c5e8fe2492a1c795f4
Suman Das <sum4ndas@gmail.com>
2024-07-26 11:23:21 +0530

    Update app.js to serve static assets

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const indexRouter = require("./routes/indexRouter");
 const formRouter = require("./routes/formRouter");
 
@@ -8,6 +9,9 @@ app.set("view engine", "ejs");
 
 // Middleware to parse form data into req.body.
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static assets.
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/new", formRouter);
