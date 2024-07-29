926d26f23405ff5d439f96aed0150c1acaf005ab
Suman Das <sum4ndas@gmail.com>
2024-07-29 16:08:14 +0530

    Update pool.js with environment variables

--- db/pool.js
+++ db/pool.js
@@ -3,6 +3,5 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  connectionString:
-    "postgresql://sdas:process.env.DB_PASS@localhost:5432/mini_messageboard",
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_DATABASE}`,
 });
