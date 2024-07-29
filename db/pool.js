ca9ed6285c515402d1bc4e17c74b1681cbf4c188
Suman Das <sum4ndas@gmail.com>
2024-07-29 20:21:18 +0530

    Update pool.js for neon database deployment

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,21 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_DATABASE}`,
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
 });
+
+module.exports = pool;
+
+// module.exports = new Pool({
+//   connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:5432/${process.env.DB_DATABASE}`,
+// });
